43c4ac610fff22a89b9b29d9327c25a61db4f69b
André Cabral <cabral.gava@gmail.com>
2022-06-21 18:15:25 -0300

    meu primeiro commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let aula = ["Matemática", "Portugues", "Ingles", "Espanhol"];
+
+for(i = 0; i < aula.length; i++){
+    if (aula.i = "Ingles") {
+        console.log("I Love speake " + aula.i);
+    }
+}
